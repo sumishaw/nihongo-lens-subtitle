dc2b0895d87d0948d8e6aa142a434aec5b43875a
sumishaw <55247599+sumishaw@users.noreply.github.com>
2026-05-12 01:09:10 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:speech_to_text/speech_to_text.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const NihongoLensApp());
@@ -18,83 +18,47 @@ class NihongoLensApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
+  static const platform =
+      MethodChannel('nihongo_lens/audio');
 
-class _HomePageState extends State<HomePage> {
-  final SpeechToText speech = SpeechToText();
+  Future<void> startCapture() async {
 
-  bool isListening = false;
-  String detectedText = "Waiting for Japanese speech...";
+    try {
 
-  Future<void> startListening() async {
-    bool available = await speech.initialize();
+      await platform.invokeMethod(
+        'startCapture',
+      );
 
-    if (available) {
-      setState(() {
-        isListening = true;
-      });
+    } catch (e) {
 
-      speech.listen(
-        localeId: 'ja_JP',
-        partialResults: true,
-        onResult: (result) {
-          setState(() {
-            detectedText = result.recognizedWords;
-          });
-        },
+      debugPrint(
+        "Capture Error: $e",
       );
     }
   }
 
-  Future<void> stopListening() async {
-    await speech.stop();
-
-    setState(() {
-      isListening = false;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Nihongo Lens"),
+        title: const Text(
+          'Nihongo Lens',
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(
-              Icons.mic,
-              size: 100,
-              color: Colors.cyan,
-            ),
-            const SizedBox(height: 30),
-            Text(
-              detectedText,
-              textAlign: TextAlign.center,
-              style: const TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(height: 40),
-            ElevatedButton(
-              onPressed:
-                  isListening ? stopListening : startListening,
-              child: Text(
-                isListening
-                    ? "STOP LISTENING"
-                    : "START LISTENING",
-              ),
-            )
-          ],
+
+      body: Center(
+
+        child: ElevatedButton(
+
+          onPressed: startCapture,
+
+          child: const Text(
+            'START INTERNAL AUDIO CAPTURE',
+          ),
         ),
       ),
     );
