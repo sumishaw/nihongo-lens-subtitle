5f461af29e618c1d5e018d8eff37805c3c7856ad
sumishaw <55247599+sumishaw@users.noreply.github.com>
2026-05-12 07:58:58 +0530

    Update TranslatorManager.kt

--- android/app/src/main/kotlin/com/example/nihongolens/TranslatorManager.kt
+++ android/app/src/main/kotlin/com/example/nihongolens/TranslatorManager.kt
@@ -1,6 +1,8 @@
 package com.example.nihongolens
 
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import com.google.mlkit.nl.translate.TranslateLanguage
 import com.google.mlkit.nl.translate.Translation
 import com.google.mlkit.nl.translate.Translator
@@ -8,39 +10,29 @@ import com.google.mlkit.nl.translate.TranslatorOptions
 
 class TranslatorManager(context: Context) {
 
-    private var translator: Translator
+    private val translator: Translator
+    private val handler = Handler(Looper.getMainLooper())
 
     init {
-
-        val options =
-            TranslatorOptions.Builder()
-                .setSourceLanguage(
-                    TranslateLanguage.JAPANESE
-                )
-                .setTargetLanguage(
-                    TranslateLanguage.ENGLISH
-                )
-                .build()
-
-        translator =
-            Translation.getClient(options)
-
+        val options = TranslatorOptions.Builder()
+            .setSourceLanguage(TranslateLanguage.JAPANESE)
+            .setTargetLanguage(TranslateLanguage.ENGLISH)
+            .build()
+        translator = Translation.getClient(options)
         translator.downloadModelIfNeeded()
     }
 
-    fun translate(
-        text: String,
-        callback: (String) -> Unit
-    ) {
-
+    fun translate(text: String, callback: (String) -> Unit) {
         translator.translate(text)
-            .addOnSuccessListener {
-
-                callback(it)
+            .addOnSuccessListener { english ->
+                handler.post { callback(english) }
             }
             .addOnFailureListener {
-
-                callback(text)
+                handler.post { callback(text) }
             }
+    }
+
+    fun close() {
+        try { translator.close() } catch (_: Exception) {}
     }
 }
